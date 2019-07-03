5d4344aaf1accce4e42ff6af51bd101fcb73c993
Kumar <sachin.am.kumar@accenture.com>
2019-07-03 15:40:08 +0530

    colour corrected

--- plot3.R
+++ plot3.R
@@ -7,9 +7,9 @@ twoDays$dateTime <- paste(twoDays$Date, twoDays$Time, sep = " ")
 twoDays$dateTime <- strptime(twoDays$dateTime, "%d/%m/%Y %H:%M:%S")
 
 plot(twoDays$dateTime, twoDays$Sub_metering_1, type = 'l', col='black',
-     ylab = "Energy sub metering")
+     xlab = "",ylab = "Energy sub metering")
 lines(twoDays$dateTime, twoDays$Sub_metering_2, col='red')
-lines(twoDays$dateTime, twoDays$Sub_metering_3, col='green')
+lines(twoDays$dateTime, twoDays$Sub_metering_3, col='blue')
 
 dev.copy(png, 'plot3.png')
 
