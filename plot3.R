124b5085e86b22beacff615f626d83bd005fa92b
Kumar <sachin.am.kumar@accenture.com>
2019-07-03 15:15:56 +0530

    plot 3

---
+++ plot3.R
@@ -0,0 +1,16 @@
+hpcData <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?")
+
+twoDays <- subset(hpcData, hpcData$Date == '1/2/2007'|hpcData$Date == '2/2/2007')
+
+twoDays$dateTime <- paste(twoDays$Date, twoDays$Time, sep = " ")
+
+twoDays$dateTime <- strptime(twoDays$dateTime, "%d/%m/%Y %H:%M:%S")
+
+plot(twoDays$dateTime, twoDays$Sub_metering_1, type = 'l', col='black',
+     ylab = "Energy sub metering")
+lines(twoDays$dateTime, twoDays$Sub_metering_2, col='red')
+lines(twoDays$dateTime, twoDays$Sub_metering_3, col='green')
+
+dev.copy(png, 'plot3.png')
+
+dev.off()
